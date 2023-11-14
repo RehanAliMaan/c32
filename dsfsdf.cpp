693d26c9a38e0930f063d7b92d12066d92401a16
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2023-11-14 19:25:46 +0500

    Add files via upload

---
+++ dsfsdf.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void lahore();
+void karachi();
+int main()
+{
+	lahore();
+	karachi();
+}
+void lahore()
+{
+	cout<<"I am in lahore";
+}
+void karachi()
+{
+	cout<<"i am in Karachi";
+}
